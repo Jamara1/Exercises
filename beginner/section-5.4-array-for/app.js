be86ae71f1ee3e2eb49036e8d429df6632766654
Jamara1 <7.6amarajohan@gmail.com>
2022-04-06 09:04:43 -0500

    Section 5.4 added

---
+++ beginner/section-5.4-array-for/app.js
@@ -0,0 +1,7 @@
+let people = ['Fernando', 'Johan', 'Francisco'];
+let salarys = [1000, 1200, 3000];
+
+for (let i = 0; i < people.length; i++) {
+    const element = people[i];
+    console.log(element + ' have salary the ' + salarys[i]);
+}
