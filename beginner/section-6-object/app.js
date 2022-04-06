474e4b9566bdfabb91f3b13fb8bd4168cb12851c
Jamara1 <7.6amarajohan@gmail.com>
2022-04-06 09:05:17 -0500

    Section 6 added

---
+++ beginner/section-6-object/app.js
@@ -0,0 +1,11 @@
+let people = {
+    name: 'Johan',
+    age: 22,
+    height: 1.79
+};
+
+function describePerson(person) {
+    console.log(`${person.name} have ${person.age} years old and is ${person.height} meters tall`);
+}
+
+describePerson(people);
