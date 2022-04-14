165775ae05df4fb5ddbd362beb53ea6ee5af4de6
Jamara1 <7.6amarajohan@gmail.com>
2022-04-13 19:04:07 -0500

    Comment added

--- typescript-beginner/export.ts
+++ typescript-beginner/export.ts
@@ -1,1 +1,2 @@
+/* Set the value that is send to the import.ts file */
 export const name = "Hello world!";
--- typescript-beginner/import.ts
+++ typescript-beginner/import.ts
@@ -1,3 +1,4 @@
+/* Get the value of the export.ts file */
 import {name} from './export';
 
 console.log(name);
