7722260d70ad710f39fb2cb1aaf0bacf73d2ac09
Jamara1 <7.6amarajohan@gmail.com>
2022-04-16 19:44:35 -0500

    footer a folderand files were created and the body files were modified

--- angular/01-hello-world/src/app/components/body/body.component.ts
+++ angular/01-hello-world/src/app/components/body/body.component.ts
@@ -2,6 +2,16 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-body',
-  templateUrl: './body.component.html'
+  templateUrl: './body.component.html',
 })
-export class BodyComponent {}
+export class BodyComponent {
+
+  status: boolean = true;
+
+  phrase: any = {
+    message: 'Great power requires great responsibility',
+    author: 'Ben Parker'
+  };
+
+  characters: string[] = ['Spiderma', 'Venom', 'Dr. Octopus']
+}
