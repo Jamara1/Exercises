be61cf51453474aa8e05ac53178d38b0234f7d70
Jamara1 <7.6amarajohan@gmail.com>
2022-04-19 18:53:58 -0500

    Modified components files
    - home.component.html to add the countries
    - home.component.ts to add the countries by API
    - navbar.component.html to add the navbar with its paths

--- angular/04-spotiapp/src/app/components/home/home.component.ts
+++ angular/04-spotiapp/src/app/components/home/home.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,7 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  countries: any[] = [];
+
+  constructor( private http: HttpClient ) {
+    this.http.get('https://restcountries.com/v3.1/lang/spa')
+    .subscribe((response: any) => {
+      this.countries = response;
+    });
+  }
 
   ngOnInit(): void {
   }
