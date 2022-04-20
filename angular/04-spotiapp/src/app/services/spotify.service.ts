1a55ba40712f6b7529afcfa2a2ec86306ddd653b
Jamara1 <7.6amarajohan@gmail.com>
2022-04-19 20:44:35 -0500

    Modified spotify.service.ts to refresh token and add getArtist method

--- angular/04-spotiapp/src/app/services/spotify.service.ts
+++ angular/04-spotiapp/src/app/services/spotify.service.ts
@@ -6,16 +6,24 @@ import { Injectable } from '@angular/core';
 })
 export class SpotifyService {
 
-  constructor(private http: HttpClient) {
-    console.log('Spotify successfully!');
-  }
+  constructor(private http: HttpClient) { }
 
   getNewRealease() {
 
     const headers: HttpHeaders = new HttpHeaders({
-      'Authorization': 'Bearer BQDsge9CSO2zGEzxzeruvfhgCh3vBqLeU4wTi19TK8avooeMQkwTSNHyFBvULdZBXY4FCuHIwATBwauWaz4'
+      'Authorization': 'Bearer BQB_BnZWfLu7AlmPVnCbKudAe7QY997Jbgm4TtVqly_Ve7lYRn4djQ65AlBqPWx71HOJY7zf-WA7fiHXmdQ'
     });
 
-    return this.http.get('	https://api.spotify.com/v1/browse/new-releases?limit=20', { headers: headers });
+    return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', { headers: headers });
+
+  }
+
+  getArtist(text: string) {
+
+    const headers: HttpHeaders = new HttpHeaders({
+      'Authorization': 'Bearer BQAS7nMLT9pSvJVhDV8LmszdK7SuEqsORBW6fY6i5TPr0KwvnLR9ZWixvx-ucodHoHOZk-7yuv5SrPg51gI'
+    });
+
+    return this.http.get(`https://api.spotify.com/v1/search?q=${text}&type=artist&limit=15`, { headers: headers });
   }
 }
