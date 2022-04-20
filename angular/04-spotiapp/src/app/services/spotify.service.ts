6fae3aca09bd1de959dda25ab1321912873339c0
Jamara1 <7.6amarajohan@gmail.com>
2022-04-19 21:03:09 -0500

    Optimized spotify.service.ts file

--- angular/04-spotiapp/src/app/services/spotify.service.ts
+++ angular/04-spotiapp/src/app/services/spotify.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,22 +9,24 @@ export class SpotifyService {
 
   constructor(private http: HttpClient) { }
 
-  getNewRealease() {
+  getQuery(query: string) {
+    const url: string = `https://api.spotify.com/v1/${query}`;
 
     const headers: HttpHeaders = new HttpHeaders({
       'Authorization': 'Bearer BQB_BnZWfLu7AlmPVnCbKudAe7QY997Jbgm4TtVqly_Ve7lYRn4djQ65AlBqPWx71HOJY7zf-WA7fiHXmdQ'
     });
 
-    return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', { headers: headers });
+    return this.http.get(url, { headers: headers });
 
   }
 
+  getNewRealease() {
+    return this.getQuery('browse/new-releases?limit=20')
+    .pipe( map((data: any) => data['albums'].items));
+  }
+
   getArtist(text: string) {
-
-    const headers: HttpHeaders = new HttpHeaders({
-      'Authorization': 'Bearer BQAS7nMLT9pSvJVhDV8LmszdK7SuEqsORBW6fY6i5TPr0KwvnLR9ZWixvx-ucodHoHOZk-7yuv5SrPg51gI'
-    });
-
-    return this.http.get(`https://api.spotify.com/v1/search?q=${text}&type=artist&limit=15`, { headers: headers });
+    return this.getQuery(`search?q=${text}&type=artist&limit=15`)
+      .pipe(map((data: any) => data['artists'].items));
   }
 }
