220efc9a67a74714257cd8c3e226ac7cb2b30012
Jamara1 <7.6amarajohan@gmail.com>
2022-04-22 01:00:37 -0500

    Created integer pipe to transform a decimal to integer without proximity

---
+++ angular/04-spotiapp/src/app/pipes/integer.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'integer'
+})
+export class IntegerPipe implements PipeTransform {
+
+  transform(value: number): number {
+    return Math.trunc(value);
+  }
+
+}
