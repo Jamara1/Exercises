e5f56f3b46da98c9ce9758771dfee7eb472a1fe6
Jamara1 <7.6amarajohan@gmail.com>
2022-04-22 00:33:41 -0500

    Added player patch

--- angular/04-spotiapp/src/app/app-routing.module.ts
+++ angular/04-spotiapp/src/app/app-routing.module.ts
@@ -1,13 +1,17 @@
+import { PlayerComponent } from './components/player/player.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { SearchComponent } from './components/search/search.component';
 import { ArtistComponent } from './components/artist/artist.component';
+import { MyLibraryComponent } from './components/my-library/my-library.component';
 
 const ROUTES: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'search', component: SearchComponent },
   { path: 'artist/:id', component: ArtistComponent },
+  { path: 'my-library', component: MyLibraryComponent },
+  { path: 'player/:id', component: PlayerComponent },
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: '**', pathMatch: 'full', redirectTo: 'home' },
 ];
