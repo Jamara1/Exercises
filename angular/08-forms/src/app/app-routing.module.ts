7ce4b1bede49cef53267d2e8373e556a87ba2924
Jamara1 <7.6amarajohan@gmail.com>
2022-04-28 00:04:00 -0500

    FormsModule added

--- angular/08-forms/src/app/app-routing.module.ts
+++ angular/08-forms/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
+import { ReactiveComponent } from './pages/reactive/reactive.component';
+import { TemplateComponent } from './pages/template/template.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const ROUTES: Routes = [
+  { path: 'template', component: TemplateComponent },
+  { path: 'reactive', component: ReactiveComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'template' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(ROUTES)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
