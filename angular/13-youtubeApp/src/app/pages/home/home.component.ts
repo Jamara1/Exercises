c7a4c095592f396ccb7e48faa27334523ce59daa
Jamara1 <7.6amarajohan@gmail.com>
2022-05-04 13:57:47 -0500

    Terminated youtube features

--- angular/13-youtubeApp/src/app/pages/home/home.component.ts
+++ angular/13-youtubeApp/src/app/pages/home/home.component.ts
@@ -1,15 +1,43 @@
+import Swal from 'sweetalert2';
 import { Component, OnInit } from '@angular/core';
+import { Video } from 'src/app/models/youtube.model';
+import { YoutubeService } from 'src/app/services/youtube.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  videos: Video[] = [];
 
-  constructor() { }
+  constructor(private _youtubeService: YoutubeService) {}
 
   ngOnInit(): void {
+    this.loadVideos();
   }
 
+  loadVideos() {
+    this._youtubeService.getVideos().subscribe((video) => {
+      this.videos.push(...video);
+    });
+  }
+
+  showVideo(video: Video) {
+    console.log(video);
+
+    Swal.fire({
+      html: `
+            <h4>${video.title}</h4>
+            <iframe width="100%"
+                     height="315"
+                     src="https://www.youtube.com/embed/${video.resourceId.videoId}"
+                     title="YouTube video player"
+                     frameborder="0"
+                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                     allowfullscreen>
+            </iframe>`,
+      allowOutsideClick: false,
+    });
+  }
 }
--- angular/13-youtubeApp/src/app/services/youtube.service.ts
+++ angular/13-youtubeApp/src/app/services/youtube.service.ts
@@ -1,9 +1,39 @@
+import { Item } from './../models/youtube.model';
 import { Injectable } from '@angular/core';
 
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Video, YoutubeResponse } from '../models/youtube.model';
+
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class YoutubeService {
+  private youtubeUrl = 'https://www.googleapis.com/youtube/v3';
+  private apiKey = 'AIzaSyBsX6OXpZ7mEb4BFGSvsP_qYAsC518npZs';
+  private playList = 'UUuaPTYj15JSkETGnEseaFFg';
+  private nextPageToken = '';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
+
+  getVideos(): Observable<Video[]> {
+    const url = `${this.youtubeUrl}/playlistItems`;
+
+    const params = new HttpParams()
+      .set('part', 'snippet')
+      .set('maxResults', 10)
+      .set('playlistId', this.playList)
+      .set('key', this.apiKey)
+      .set('pageToken', this.nextPageToken);
+
+    return this.httpClient.get<YoutubeResponse>(url, { params }).pipe(
+      map(response => {
+        this.nextPageToken = response.nextPageToken;
+        return response.items;
+      }),
+      map((items) => items.map((video: Item) => video.snippet))
+    );
+  }
 }
