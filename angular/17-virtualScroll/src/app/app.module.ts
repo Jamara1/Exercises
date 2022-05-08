817d5eb748b62f4055714427e9c472031a66945c
Jamara1 <7.6amarajohan@gmail.com>
2022-05-08 17:03:31 -0500

    Created virtual components

--- angular/17-virtualScroll/src/app/app.module.ts
+++ angular/17-virtualScroll/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { VitualComponent } from './components/vitual/vitual.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VitualComponent
   ],
   imports: [
     BrowserModule
---
+++ angular/17-virtualScroll/src/app/components/vitual/vitual.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vitual',
+  templateUrl: './vitual.component.html',
+  styles: [
+  ]
+})
+export class VitualComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
