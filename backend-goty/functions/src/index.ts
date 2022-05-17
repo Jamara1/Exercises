de543cc40313f9b4d0250b20c0e9aa6dc978ee36
Jamara1 <johanamara@fundacionview.org>
2022-05-17 15:46:39 -0500

    Problem when continuing with the backend, functions are no longer free of charge

--- backend-goty/functions/src/index.ts
+++ backend-goty/functions/src/index.ts
@@ -1,4 +1,12 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+
+const serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://firestore-grafica.firebaseio.com"
+});
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -6,4 +14,12 @@ import * as functions from "firebase-functions";
 export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello world from Firebase functions!");
+});
+
+export const getGoty = functions.https.onRequest((request, response) => {
+  const name = request.query.name || 'No name';
+
+  response.send({
+    name
+  });
 });
